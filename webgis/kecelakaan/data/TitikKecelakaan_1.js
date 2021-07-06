d30a39d51286969579c67767b5ca4691384751bf
arsha09 <arsha.trikusuma88@gmail.com>
2021-07-07 06:35:21 +0700

    first commit

---
+++ webgis/kecelakaan/data/TitikKecelakaan_1.js
@@ -0,0 +1,24 @@
+var json_TitikKecelakaan_1 = {
+"type": "FeatureCollection",
+"name": "TitikKecelakaan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "titik_laka": "Pertigaan Jalan Hanoman-Jalan Siliwangi, Kota Semarang" }, "geometry": { "type": "Point", "coordinates": [ 110.374650346792492, -6.986639591267144 ] } },
+{ "type": "Feature", "properties": { "id": "2", "titik_laka": "Jalan Gombel Lama, Kota Semarang" }, "geometry": { "type": "Point", "coordinates": [ 110.418716449839351, -7.039347984712615 ] } },
+{ "type": "Feature", "properties": { "id": "3", "titik_laka": "Jalan Walisongo, Kota Semarang" }, "geometry": { "type": "Point", "coordinates": [ 110.358362244918013, -6.986219633421682 ] } },
+{ "type": "Feature", "properties": { "id": "4", "titik_laka": "Jalan Kaligawe Raya, Kota Semarang" }, "geometry": { "type": "Point", "coordinates": [ 110.447466984970106, -6.958119296183487 ] } },
+{ "type": "Feature", "properties": { "id": "5", "titik_laka": "Jalan Perintis Kemerdekaan, Kota Semarang" }, "geometry": { "type": "Point", "coordinates": [ 110.408764554054457, -7.089536631085872 ] } },
+{ "type": "Feature", "properties": { "id": "6", "titik_laka": "Jalan Pati - Kudus KM 8, Kab. Kudus" }, "geometry": { "type": "Point", "coordinates": [ 110.91537738546613, -6.805963779781026 ] } },
+{ "type": "Feature", "properties": { "id": "7", "titik_laka": "Jalan Pati - Juwana KM 3, Kab. Pati" }, "geometry": { "type": "Point", "coordinates": [ 111.116295901871439, -6.725360423218853 ] } },
+{ "type": "Feature", "properties": { "id": "8", "titik_laka": "Jalan Pati - Rembang KM 17, Kab. Pati" }, "geometry": { "type": "Point", "coordinates": [ 111.22154323208737, -6.70892312579416 ] } },
+{ "type": "Feature", "properties": { "id": "9", "titik_laka": "Jalan Pati - Rembang Desa Batursari, Kab. Rembang" }, "geometry": { "type": "Point", "coordinates": [ 111.231689450472018, -6.708050976496692 ] } },
+{ "type": "Feature", "properties": { "id": "10", "titik_laka": "Jalan Pahlawan, Kab. Banyumas" }, "geometry": { "type": "Point", "coordinates": [ 109.224072681638546, -7.431863979459444 ] } },
+{ "type": "Feature", "properties": { "id": "11", "titik_laka": "Jalan Raya Sidabowa, Karanggede, Kab. Banyumas" }, "geometry": { "type": "Point", "coordinates": [ 109.219865735502495, -7.463732885784185 ] } },
+{ "type": "Feature", "properties": { "id": "12", "titik_laka": "Jalan Solo – Karanganyar, Ds. Palur, Kecamatan Jaten, Kab. Karanganyar" }, "geometry": { "type": "Point", "coordinates": [ 110.877070967433525, -7.570803717171149 ] } },
+{ "type": "Feature", "properties": { "id": "13", "titik_laka": "Jalan Lawu Ds. Dagen, Kecamatan Jaten, Kab. Karanganyar" }, "geometry": { "type": "Point", "coordinates": [ 110.873726040032892, -7.569197562604326 ] } },
+{ "type": "Feature", "properties": { "id": "14", "titik_laka": "Jalan Solo – Tawangmangu Ds. Papahan, Kecamatan Tasikmadu, Kab. Karanganyar" }, "geometry": { "type": "Point", "coordinates": [ 110.921063393665179, -7.587447309677815 ] } },
+{ "type": "Feature", "properties": { "id": "15", "titik_laka": "Jalan Lawu 172 Ds. Bejen-Tegalgede, Kab. Karanganyar" }, "geometry": { "type": "Point", "coordinates": [ 110.964066708655579, -7.600915431458334 ] } },
+{ "type": "Feature", "properties": { "id": "16", "titik_laka": "Jalan Raya Sukoharjo – Wonogiri Ds. Kepuh, Kecamatan Nguter, Kab. Sukoharjo" }, "geometry": { "type": "Point", "coordinates": [ 110.860607883123606, -7.71324310359497 ] } },
+{ "type": "Feature", "properties": { "id": "17", "titik_laka": "Jalan Raya Sukoharjo – Wonogiri, depan ayam goreng Mbak Mulyani, Kab. Sukoharjo" }, "geometry": { "type": "Point", "coordinates": [ 110.852138733240309, -7.699907600081438 ] } }
+]
+}
